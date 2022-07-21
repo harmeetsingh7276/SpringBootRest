91d6a2c3e40a96381689f5847279dc889de8a910
harmeet <harmeetsingh7276@gmail.com>
2022-07-22 01:57:04 +0530

    First Commit

---
+++ SpringDataRest/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO alien VALUES (1, 'Harmeet','Java');
+INSERT INTO alien VALUES (2, 'Payal','Kotlin');
+INSERT INTO alien VALUES (3, 'Papa','C');
+INSERT INTO alien VALUES (4, 'Mummy','C++');
+INSERT INTO alien VALUES (5, 'Papaji','Python');
+INSERT INTO alien VALUES (6, 'Mummyji','Android');
